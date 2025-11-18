804c1a8213662cf3bc2b3cd2840ea121c7a88684
Diego Silva <diegosillvaa01@gmail.com>
2025-11-17 23:23:44 -0300

    __init__.py

--- campus/__init__.py
+++ campus/__init__.py
@@ -1,3 +1,1 @@
-# Este arquivo indica que a pasta 'campus' é um pacote Python.
-# Ele permite iplementar um código baseado em listas que permita fazer o CRUD (create-read-update-delete).
-# (Pode ser deixado vazio ou conter importações de conveniência.)
+
